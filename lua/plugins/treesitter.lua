7753393ec9802549fea420991643b55d8104c46a
rivo <rivoten05@gmail.com>
2025-12-25 15:54:14 +0530

    fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,8 +4,7 @@ return {
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
+			require("nvim-treesitter").setup({
 				auto_install = true,
 				highlight = { enable = true, additional_vim_regex_highlighting = false },
 				indent = { enable = true },
